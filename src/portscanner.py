80dd892f756ce467126f25996a20cb1f136bd617
Edgar Coime <e.coimeii@hotmail.com>
2024-11-06 22:49:23 -0800

    feat(scanner): basic packet construction

--- src/portscanner.py
+++ src/portscanner.py
@@ -1,9 +1,22 @@
 import sys
 import time
+from enum import Enum
 
-from scapy.all import IP, TCP, conf, sr1
+from scapy.layers.inet import ICMP, IP, TCP
+from scapy.sendrecv import conf, sr1
 
 from argparser import Parser
+
+
+class PortStatus(Enum):
+    OPEN = "OPEN"
+    CLOSED = "CLOSED"
+    FILTERED = "FILTERED"
+    UNKNOWN = "UNKNOWN"
+
+
+TIMEOUT_SEC = 2
+RETRIES = 2
 
 
 class PortScanner:
@@ -13,6 +26,17 @@ class PortScanner:
     def scan(self):
         config = self.config
         print("Scanning Target: ", self.config.target)
+        self.scan_port(config.target, 22)
+
+    def scan_port(
+        self, target: str, port: int, timeout=TIMEOUT_SEC, retries=RETRIES
+    ) -> PortStatus:
+        conf.verb = 0
+
+        ip = IP(dst=target)
+        tcp = TCP(dport=port, flags="S")
+
+        return PortStatus.UNKNOWN
 
     def __str__(self) -> str:
         return f"config: {self.config}"
